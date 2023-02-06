bb6325d5738cc22d4a2290ff6e1f8c9a35d7094c
Mahmoud Selim <65083562+Mahmoudelim@users.noreply.github.com>
2023-02-06 16:11:40 +0200

    UpdateUI

--- app/src/main/java/com/example/emergency_alert_system/MainActivity.kt
+++ app/src/main/java/com/example/emergency_alert_system/MainActivity.kt
@@ -3,16 +3,17 @@ package com.example.emergency_alert_system
 import android.Manifest
 import android.content.Intent
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.example.emergency_alert_system.user.UserDachboard
 import com.example.emergencyalertsystem.R
 
 class MainActivity : AppCompatActivity() {
+    private var LocationPermissionGranted = false
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,13 +23,14 @@ class MainActivity : AppCompatActivity() {
         ) { permissions ->
             when {
                 permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-                    // Precise location access granted.
+                    LocationPermissionGranted = true
                 }
                 permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
-                    // Only approximate location access granted.
-                } else -> {
-                // No location access granted.
-            }
+                    LocationPermissionGranted = true
+                }
+                else -> {
+                    // No location access granted.
+                }
             }
         }
 
@@ -37,20 +39,25 @@ class MainActivity : AppCompatActivity() {
 // Before you perform the actual permission request, check whether your app
 // already has the permissions, and whether your app needs to show a permission
 // rationale dialog. For more details, see Request permissions.
-        locationPermissionRequest.launch(arrayOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION))
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
 
-        val btn: Button =findViewById(R.id.login_btn)
+        val btn: Button = findViewById(R.id.login_btn)
         //logout
-        btn.setOnClickListener(object : View.OnClickListener{
-            override fun onClick(v: View?) {
+        if (LocationPermissionGranted == true) {
+            btn.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(v: View?) {
 
-                val intent= Intent(this@MainActivity, UserDachboard::class.java)
-                startActivity(intent);
+                    val intent = Intent(this@MainActivity, UserDachboard::class.java)
+                    startActivity(intent);
 
-            }
+                }
 
-        })
+            })
+        }
     }
 }
