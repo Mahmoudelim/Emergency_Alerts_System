8edaa85c6f898941f4bfc2280dc7fbb45f243808
abdelrhmannagi <abdelrhmannagi10@gmail.com>
2023-02-07 17:39:50 +0200

    for mahmoud2

---
+++ app/src/main/java/com/example/emergency_alert_system/user/creation/user_location.kt
@@ -0,0 +1,7 @@
+package com.example.emergency_alert_system.user.creation
+
+class user_location(var username:String,var streetname:String,var naighbourrhood:String,var buildingnumb :Int,var floornumb :Int,var flatingnumb :Int
+):user(username) {
+}
+//onot forget cuurent location
+//location on home
---
+++ app/src/main/java/com/example/emergency_alert_system/user/creation/user_vitaldata.kt
@@ -0,0 +1,4 @@
+package com.example.emergency_alert_system.user.creation
+
+class user_vitaldata(var username:String,var pulserate:String,var tempertuture:Int,var pressure: Int):user(username) {
+}
