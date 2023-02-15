e14c2ba1f7e3d29cf73ec86cae6de3a591e39d3c
Mahmoud Selim <65083562+Mahmoudelim@users.noreply.github.com>
2023-02-15 20:48:49 +0200

    finish assign doctor and update medicines

--- app/src/main/java/com/example/emergency_alert_system/Doctor/Creation/Doctor.kt
+++ app/src/main/java/com/example/emergency_alert_system/Doctor/Creation/Doctor.kt
@@ -1,6 +1,6 @@
 package com.example.emergency_alert_system.Doctor.Creation
 
 data class Doctor(var Name: String?=null, var email: String?=null
-                  , var specilization: String?=null, var qualification:String?=null, var password : String?=null )
+                  , var spicialization: String?=null, var qualification:String?=null, var password : String?=null )
  // collection to get  patients name  on list
 // name
