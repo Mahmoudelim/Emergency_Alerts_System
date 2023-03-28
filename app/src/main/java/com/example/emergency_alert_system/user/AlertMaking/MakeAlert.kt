3c51559dcd3dd966b56db2125d994a0dd099e23a
Mahmoud Selim <65083562+Mahmoudelim@users.noreply.github.com>
2023-03-28 17:26:12 +0200

    final Make Alert to Ep

--- app/src/main/java/com/example/emergency_alert_system/user/AlertMaking/MakeAlert.kt
+++ app/src/main/java/com/example/emergency_alert_system/user/AlertMaking/MakeAlert.kt
@@ -5,13 +5,10 @@ import android.widget.Toast
 import com.example.emergency_alert_system.EMP.creation.Emergency_point
 import com.example.emergency_alert_system.user.creation.user_location
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.CollectionReference
-import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.DocumentSnapshot
-import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.*
 
 var nm :String = ""
-
+var Nearest_EP:String=""
 class MakeAlert {
     //login user name
    var mAuth: FirebaseAuth = FirebaseAuth.getInstance()
@@ -37,92 +34,95 @@ class MakeAlert {
             }
         }
     }
-    val result=ArrayList<Emergency_point>()
+    var result= mutableListOf<Emergency_point>()
+    fun nearestGPSCoridnate(userLocation: user_location, neighborhood:String) :String{
+       var nearestEP = Emergency_point()
+       var nearestEPName=""
+       val neighborhoodRef = firestore.collection("Emergency point").whereEqualTo("naighbourhood", neighborhood)
+       neighborhoodRef.get().addOnCompleteListener { task ->
+           if (task.isSuccessful) {
+               val documentSnapshot = task.result
+               if (documentSnapshot != null) {
+                    result = documentSnapshot.toObjects(Emergency_point::class.java)!!
+                   Log.i("nearestEPArray", result.toString())
+                   nearestEP=result[0]
+                   Log.i("nearestEP", nearestEP.toString())
+                   for (EP in result) {
+                       val lat = userLocation.latitude
+                       Log.i("lat in usesrLoc",lat.toString())
+                       Log.i("nearestEP1", nearestEP.toString())
+                       val long = userLocation.longitude
+                       Log.i("long in usesrLoc",long.toString())
+                       Log.i("nearestEP2", nearestEP.toString())
+                       val targetlat = EP.latitude
+                       Log.i("target lat in usesrLoc",targetlat.toString())
+                       Log.i("nearestEP3", nearestEP.toString())
+                       val targetLong = EP.longitude
+                       Log.i("target long in usesrLoc",targetLong.toString())
+                       Log.i("nearestEP4", nearestEP.toString())
+                       val nearestEPLat=nearestEP!!.latitude
+                       Log.i("nearestEPlat", nearestEPLat.toString())
+                       val nearestEPLong=nearestEP.longitude
+                       Log.i("nearestEPlong", nearestEPLong.toString())
+                       if (lat!!.minus(targetlat!!) < nearestEPLat!!.minus(targetlat)!!
+                           && long!!.minus(targetLong!!) < nearestEPLong!!.minus(targetLong)!!)
+                       {
+                           Log.i("nearestEP5", nearestEP.toString())
+                           nearestEP = EP
+                           Log.i("nearestEP6", nearestEP.toString())
+                       }
+                   }
 
-        fun nearestGPSCoridnate(userLocation: user_location,neighborhood:String):String{
 
-            //find nearest in neighborhood
-            //array take all EP in this neighborhood
-            var nearestEP =Emergency_point()
-            Log.i("neeee",neighborhood.toString())
-            val neighborhoodRef= firestore.collection("Emergency point")
-                .whereEqualTo("naighbourhood".trim(), neighborhood)
-                .addSnapshotListener{ snapshot,exception ->
-                    if(exception !=null)
-                    {
-                        Log.e("snapshotException",exception.toString())
-                        return@addSnapshotListener
-                    }
-                    if (snapshot!=null){
-                        for (document in snapshot.documents){
-                            var doc=document.toObject(Emergency_point::class.java)
-                            result.add(doc!!)
-                            Log.i("neArray",result.toString())
+                   // move the return statement here
+                    nearestEPName = nearestEP.EPName.toString()
+                   Log.i("nearestEPName", nearestEPName.toString())
+                    Nearest_EP=nearestEP.EPName.toString()
+                   return@addOnCompleteListener
+               }
+           }
+           //
 
-                        }
-                    }
+           // iterate in this list
 
 
-
-
-                     Log.i("nearest", result.toString())
-
-
-              }
-
-            Log.i("nearest", result.toString())
-
-            // iterate in this list
-            for(EP in result!!)
-            {
-
-                var lat = userLocation.latitude
-                var long= userLocation.longitude
-                var targetlat=EP.latitude
-                var targetLong=EP.longitude
-                if (lat!!.minus(targetlat!!) < nearestEP.latitude!!.minus(targetlat!!)
-                    &&long!!.minus(targetLong!!) < nearestEP.longitude!!.minus(targetLong))
-                {
-                    nearestEP =EP
-
+                    Log.i("nearest", nearestEP.toString())
                 }
-            }
-        return nearestEP!!.EPName!!
-        }
 
 
-    fun findNearestEP( userLocation: user_location ): String? {
-           var nearestEp: String? =null
-        if (userLocation.state=="Cairo".trim())
-        {
-          if(userLocation.naighbourrhood=="Badr City".trim())
-          {
-              nearestEp=nearestGPSCoridnate(userLocation,"Badr City")
-             // nearestEp="Badr"
-          }
-            else if (userLocation.naighbourrhood=="Elmarg")
-          {
-              nearestEp=nearestGPSCoridnate(userLocation,"Elmarg")
-             //  nearestEp="Elmarg"
-            }
 
-        }
-        else if (userLocation.state=="Alex")
-        {
-
-        }
-    return nearestEp
-    }
-    fun AlertToEp(){
-        val alertMap= hashMapOf(
-            "name" to nm
+           // return an empty string for now
 
 
-        )
-        val nearestEp: String? = userLocation?.let { findNearestEP(it) }
-        firestore.collection("$nearestEp requests").document().set(alertMap)
 
-    }
-    fun AlertToRelatives(){}
-    fun AlertToDoc(){}
-}
+      return Nearest_EP
+   }
+
+       fun findNearestEP(userLocation: user_location): String? {
+           var nearestEp: String? = null
+           if (userLocation.state == "Cairo".trim()) {
+
+               Log.i("iiiii", userLocation.state.toString())
+               when (userLocation.naighbourrhood) {
+                   "Elmarg" -> nearestEp = nearestGPSCoridnate(userLocation, "Elmarg")
+               }
+           } else if (userLocation.state == "Alex") {
+
+           }
+           return nearestEp
+       }
+
+       fun AlertToEp() {
+           val alertMap = hashMapOf(
+               "name" to nm
+
+
+           )
+           val nearestEp: String? = userLocation?.let { findNearestEP(it) }
+           firestore.collection("$nearestEp requests").document().set(alertMap)
+
+       }
+
+       fun AlertToRelatives() {}
+       fun AlertToDoc() {}
+   }
